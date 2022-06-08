e07e99b9aaf1e94af3cda88054ff9a6edd3fe63b
HarkerKei <iris.ala.alba@gmail.com>
2022-06-08 12:55:10 +0900

    minor fixes

--- cmd/derod/rpc/rpc_dero_addresstoname.go
+++ cmd/derod/rpc/rpc_dero_addresstoname.go
@@ -24,9 +24,15 @@ func AddressToName(ctx context.Context, p rpc.AddressToName_Params) (result rpc.
 		}
 	}()
 
+	var req_addr *rpc.Address
+	if req_addr, err = rpc.NewAddress(strings.TrimSpace(p.Address)); err != nil {
+		err = fmt.Errorf("Invalid Address")
+		return
+	}
+
 	var getsc_result rpc.GetSC_Result
 	getsc_result.VariableStringKeys = map[string]interface{}{}
-	getsc_result.VariableUint64Keys = map[uint64]interface{}{}
+	//getsc_result.VariableUint64Keys = map[uint64]interface{}{}
 	getsc_result.Balances = map[string]uint64{}
 
 	scid := crypto.HashHexToHash("0000000000000000000000000000000000000000000000000000000000000001")
@@ -37,17 +43,17 @@ func AddressToName(ctx context.Context, p rpc.AddressToName_Params) (result rpc.
 		topoheight = p.TopoHeight
 	}
 
-	toporecord, err := chain.Store.Topo_store.Read(topoheight)
+	toporecord, err2 := chain.Store.Topo_store.Read(topoheight)
 	// we must now fill in compressed ring members
-	if err == nil {
+	if err2 == nil {
 		var ss *graviton.Snapshot
-		ss, err = chain.Store.Balance_store.LoadSnapshot(toporecord.State_Version)
-		if err == nil {
+		ss, err2 = chain.Store.Balance_store.LoadSnapshot(toporecord.State_Version)
+		if err2 == nil {
 			var sc_data_tree *graviton.Tree
-			sc_data_tree, err = ss.GetTree(string(scid[:]))
-			if err == nil {
+			sc_data_tree, err2 = ss.GetTree(string(scid[:]))
+			if err2 == nil {
 				var zerohash crypto.Hash
-				if balance_bytes, err := sc_data_tree.Get(zerohash[:]); err == nil {
+				if balance_bytes, err2 := sc_data_tree.Get(zerohash[:]); err2 == nil {
 					if len(balance_bytes) == 8 {
 						getsc_result.Balance = binary.BigEndian.Uint64(balance_bytes[:])
 					}
@@ -55,7 +61,7 @@ func AddressToName(ctx context.Context, p rpc.AddressToName_Params) (result rpc.
 				// user requested all variables
 				cursor := sc_data_tree.Cursor()
 				var k, v []byte
-				for k, v, err = cursor.First(); err == nil; k, v, err = cursor.Next() {
+				for k, v, err2 = cursor.First(); err2 == nil; k, v, err2 = cursor.Next() {
 					var vark, varv dvm.Variable
 
 					_ = vark
@@ -68,10 +74,12 @@ func AddressToName(ctx context.Context, p rpc.AddressToName_Params) (result rpc.
 						getsc_result.Balances[fmt.Sprintf("%x", k)] = binary.BigEndian.Uint64(v)
 					} else if k[len(k)-1] >= 0x3 && k[len(k)-1] < 0x80 && nil == vark.UnmarshalBinary(k) && nil == varv.UnmarshalBinary(v) {
 						switch vark.Type {
+						case dvm.Uint64:
+
 						case dvm.String:
 							if varv.Type == dvm.Uint64 {
-								err = fmt.Errorf("UNKNOWN Data type")
-								return
+								//err = fmt.Errorf("UNKNOWN Data type")
+								//return
 							} else {
 								getsc_result.VariableStringKeys[vark.ValueString] = fmt.Sprintf("%x", []byte(varv.ValueString))
 							}
@@ -79,25 +87,16 @@ func AddressToName(ctx context.Context, p rpc.AddressToName_Params) (result rpc.
 							err = fmt.Errorf("UNKNOWN Data type")
 							return
 						}
-
 					}
 				}
 			}
-
 		}
-
-	}
-
-	var req_addr *rpc.Address
-	if req_addr, err = rpc.NewAddress(strings.TrimSpace(p.Address)); err != nil {
-		err = fmt.Errorf("Invalid Address")
-		return
 	}
 
 	for k, v := range getsc_result.VariableStringKeys {
 		b, _ := hex.DecodeString(v.(string))
-		addr, err := rpc.NewAddressFromCompressedKeys(b)
-		if err != nil {
+		addr, err3 := rpc.NewAddressFromCompressedKeys(b)
+		if err3 != nil {
 			//fmt.Printf("%s, %s\n", k, v)
 			continue
 		}
@@ -109,7 +108,7 @@ func AddressToName(ctx context.Context, p rpc.AddressToName_Params) (result rpc.
 
 	if len(result.Names) != 0 {
 		sort.Strings(result.Names)
-		result.Address = p.Address
+		result.Address = strings.TrimSpace(p.Address)
 		result.Status = "OK"
 	} else {
 		err = fmt.Errorf("Not Found")
